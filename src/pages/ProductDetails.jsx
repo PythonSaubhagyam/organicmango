10ac80e725e6c4c74c53be602c0355dadc419e01
dixi1365 <dixi@saubhagyam.org>
2024-10-28 14:56:21 +0530

    add

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -723,7 +723,7 @@ export default function ProductDetails() {
               </Flex>
             </Container>
           )}
-
+{relatedProducts && relatedProducts?.length > 0( 
           <ProductListSection
             title="Related Products"
             products={relatedProducts}
@@ -732,7 +732,8 @@ export default function ProductDetails() {
             fontSize={{ base: "sm", lg: "md" }}
             type={"carousal"}
           />
-
+        )}
+        {otherProducts && otherProducts?.length > 0( 
           <ProductListSection
             title="Other Products"
             products={otherProducts}
@@ -741,7 +742,8 @@ export default function ProductDetails() {
             fontSize={{ base: "sm", lg: "md" }}
             type={"carousal"}
           />
-
+        )}
+        {recentlyViewedProducts && recentlyViewedProducts?.length > 0 (
           <ProductListSection
             title="Recently Viewed Products"
             products={recentlyViewedProducts}
@@ -750,7 +752,7 @@ export default function ProductDetails() {
             fontSize={{ base: "sm", lg: "md" }}
             type={"carousal"}
           />
-
+        )}
           <Modal
             size={"xl"}
             closeOnOverlayClick={false}
